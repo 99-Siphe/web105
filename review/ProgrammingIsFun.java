b2d83c47abf4e4b0a6c7a91c9f117f16c7addecc
99-Siphe <84157440+99-Siphe@users.noreply.github.com>
2021-05-20 20:27:21 +0200

    Add files via upload

---
+++ review/ProgrammingIsFun.java
@@ -0,0 +1,10 @@
+package com.gmail.bazookasiphesihle;
+import javax.swing.JOptionPane; //Programming is fun
+public class ProgrammingIsFun 
+{
+     public static void main(String [] args) 
+	{
+		JOptionPane.showMessageDialog(null,"Programming is fun");
+	    System.exit(0);
+	}
+}
